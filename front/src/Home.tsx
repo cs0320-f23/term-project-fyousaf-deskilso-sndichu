afde4fb0a5d48ec1152709d6ac8391dbae49f344
fatimayousaf14 <fatima_yousaf@brown.edu>
2023-12-15 01:58:37 +0500

    home page reorganized

--- front/src/Home.tsx
+++ front/src/Home.tsx
@@ -34,41 +34,39 @@ function Home() {
 
   return (
     // TODO (eventually): add CSS to make the app look nice
-    <div>
-      <Link to="/home">
-        <button style={{ marginLeft: "10px" }}>Home</button>
-      </Link>
-      <Link to="/recommendation">
-        <button style={{ marginLeft: "10px" }}>Recommendation</button>
-      </Link>
-      <Link to="/rating">
-        <button style={{ marginLeft: "10px" }}> Rating</button>
-      </Link>
-      <Link to="/form">
-        <button style={{ marginLeft: "10px" }}>Form</button>
-      </Link>
-      <div
-        style={{
-          textAlign: "center",
-          marginTop: "50px",
-          backgroundColor: "#b07e73",
-          padding: "30px",
-        }}
-      >
-        {/* <h1>Welcome to our app!</h1>
+
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "50px",
+        backgroundColor: "#b07e73",
+        padding: "30px",
+      }}
+    >
+      {/* <h1>Welcome to our app!</h1>
       <Form />
       <Rating />
       <Recommendation /> */}
 
-        <h1>Home Page</h1>
-        <GoogleLogin
-          clientId={googleclientID}
-          buttonText={"Login with Google"}
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          hostedDomain={"brown.edu"}
-          cookiePolicy={"single_host_origin"}
-        />
+      <h1>Home Page</h1>
+      <h2>
+        {" "}
+        Welcome to Weather! Please click on a button to navigate to your desired
+        tab
+      </h2>
+      <div>
+        <Link to="/home">
+          <button style={{ marginLeft: "10px" }}>Home</button>
+        </Link>
+        <Link to="/recommendation">
+          <button style={{ marginLeft: "10px" }}>Recommendation</button>
+        </Link>
+        <Link to="/rating">
+          <button style={{ marginLeft: "10px" }}> Rating</button>
+        </Link>
+        <Link to="/form">
+          <button style={{ marginLeft: "10px" }}>Form</button>
+        </Link>
         <div />
         <div />
       </div>
